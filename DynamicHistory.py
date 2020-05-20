9969030df848a0fbd474c65dfd6be940aeae4bec
Yogesh Yadav <yogesh7132@gmail.com>
2020-05-20 06:38:48 +0530

    Update DynamicHistory.py

--- DynamicHistory.py
+++ DynamicHistory.py
@@ -1,4 +1,20 @@
 import sqlite3
-import re
 
-conn=sqlite3.connect('C:\Users\yogesh\AppData\Local\Google\Chrome\User Data\Default')
+conn=sqlite3.connect(r"C:\Users\yy\AppData\Local\Google\Chrome\User Data\Default\History")
+cursor=conn.cursor()
+
+search_value="github"       #Search Value
+
+id=0
+id_lst=[]
+
+for row in cursor.execute("select id,url  from  urls where url like '%"+search_value+"%'"):
+    print(row)
+    id=row[0]
+    id_lst.append((id,))
+
+cursor.executemany('Delete from urls where id=?',id_lst)
+conn.commit()
+
+conn.close()
+
